1d466d9d2293c9da07818f7109b437bb663d7041
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-19 13:36:36 -0400

    Update rangeRunner.js

--- js/rangeRunner.js
+++ js/rangeRunner.js
@@ -1,14 +1,16 @@
-const rangeInputs = document.getElementsByTagName('input')
-for (var i=1; i <= rangeInputs.length; i++) {
-    var input = rangeInputs[i]
-    input.addEventListener('input', function(e) {
-        if (input.type === 'range') {
-            let target = input
-            const min = target.min
-            const max = target.max
-            const val = target.value
-
-            target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-        }
-    })
-}
+setTimeout(function(){
+    const rangeInputs = document.getElementsByTagName('input')
+    for (var i=1; i <= rangeInputs.length; i++) {
+        var input = rangeInputs[i]
+        input.addEventListener('input', function(e) {
+            if (input.type === 'range') {
+                let target = input
+                const min = target.min
+                const max = target.max
+                const val = target.value
+    
+                target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+            }
+        })
+    }
+},300)
