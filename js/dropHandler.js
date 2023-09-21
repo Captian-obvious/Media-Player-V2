9948bcef51e2321de67842333e5473009616692f
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-21 08:46:13 -0400

    Update dropHandler.js

--- js/dropHandler.js
+++ js/dropHandler.js
@@ -1,23 +1,25 @@
 window.siteData = {}
-document.addEventListener('dragover',function(event){
-    document.getElementById('drop_zone').hidden = false
+window.addEventListener('load',function() {
+    document.addEventListener('dragover',function(event){
+        document.getElementById('drop_zone').hidden = false;
+    });
+    document.addEventListener('dragleave',function(event){
+        setTimeout(function(){
+            document.getElementById('drop_zone').hidden = true
+        },1000);
+    });
+    function handleDragOver(ev){
+        dropzone.handleDragOver(ev,function(ev){
+            console.log('File(s) in drop zone');
+        });
+    };
+    function handleDrop(ev){
+        dropzone.handleDrop(ev,function(ev){
+            if (ev.dataTransfer.items) {
+                var files = dropzone.getFiles(ev.dataTransfer.items);
+                console.log(files);
+                window.siteData.files = files;
+            };
+        });
+    };
 })
-document.addEventListener('dragleave',function(event){
-    setTimeout(function(){
-        document.getElementById('drop_zone').hidden = true
-    },25)
-})
-function handleDragOver(ev){
-    dropzone.handleDragOver(ev,function(ev){
-        console.log('File(s) in drop zone');
-    })
-}
-function handleDrop(ev){
-    dropzone.handleDrop(ev,function(ev){
-        if (ev.dataTransfer.items) {
-            var files = dropzone.getFiles(ev.dataTransfer.items)
-            console.log(files)
-            window.siteData.files = files
-        }
-    })
-}
