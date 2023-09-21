164ed6c7afa9983d7c9e80b39f76109a36236e51
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-21 08:11:16 -0400

    Update and rename dropHandler to dropHandler.js

--- js/dropHandler.js
+++ js/dropHandler.js
@@ -1,4 +1,12 @@
 window.siteData = {}
+document.addEventListener('dragover',function(event){
+    document.getElementById('drop_zone').hidden = false
+})
+document.addEventListener('dragleave',function(event){
+    setTimeout(function(){
+        document.getElementById('drop_zone').hidden = true
+    },25)
+})
 function handleDragOver(ev){
     dropzone.handleDragOver(ev,function(ev){
         console.log('File(s) in drop zone');
