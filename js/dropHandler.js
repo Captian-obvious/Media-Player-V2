b858f8a4059b24fc9af3fb658db38ce852a1b7af
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-21 08:57:16 -0400

    Update dropHandler.js

--- js/dropHandler.js
+++ js/dropHandler.js
@@ -1,25 +1,24 @@
 window.siteData = {}
-window.addEventListener('load',function() {
-    document.addEventListener('dragover',function(event){
-        document.getElementById('drop_zone').hidden = false;
+document.addEventListener('dragover',function(event){
+    document.getElementById('drop_zone').hidden = false;
+});
+document.addEventListener('dragleave',function(event){
+    setTimeout(function(){
+        document.getElementById('drop_zone').hidden = true;
+    },1000);
+});
+window.funcs = {};
+funcs.handleDragOver = function(ev){
+    dropzone.handleDragOver(ev,function(ev){
+        console.log('File(s) in drop zone');
     });
-    document.addEventListener('dragleave',function(event){
-        setTimeout(function(){
-            document.getElementById('drop_zone').hidden = true
-        },1000);
+};
+funcs.handleDrop = function(ev){
+    dropzone.handleDrop(ev,function(ev){
+        if (ev.dataTransfer.items) {
+            var files = dropzone.getFiles(ev.dataTransfer.items);
+            console.log(files);
+            window.siteData.files = files;
+        };
     });
-    function handleDragOver(ev){
-        dropzone.handleDragOver(ev,function(ev){
-            console.log('File(s) in drop zone');
-        });
-    };
-    function handleDrop(ev){
-        dropzone.handleDrop(ev,function(ev){
-            if (ev.dataTransfer.items) {
-                var files = dropzone.getFiles(ev.dataTransfer.items);
-                console.log(files);
-                window.siteData.files = files;
-            };
-        });
-    };
-})
+};
