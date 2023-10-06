c05868114d4adae163a7dafff4c4c8be41b8ce9d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-06 09:17:54 -0400

    Update embed.js

--- js/embed.js
+++ js/embed.js
@@ -1,9 +1,29 @@
 const tr = window.jsmediatags;
 
-function getParams() {
-    
-}
+function getQuery(name) {
+    var val = null;
+    if (window.location.search) {
+        var currentQuery = window.location.search.split('?')[1];
+        var queryTable = currentQuery.split('&');
+        for (var i=0;i<queryTable.length;i++) {
+            if (queryTable[i].split('=')[0]!=null) {
+                var table = queryTable[i].split('=')
+                if (table[0]==name) {
+                    val = table[1];
+                    break;
+                };
+            };
+        };
+    };
+    return val;
+};
 
 window.addEventListener("load", function () {
-    
+    var controls = getQuery('controls');
+    var info = getQuery('info');
+    var vis = getQuery('visual');
+    var visType = getQuery('vType');
+    if (controls==='true') {
+        
+    };
 };
